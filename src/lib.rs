7bdfd1beb9187a8cbd1fe011758fda1c9242e7ed
Can Göktas <can.goektas@gmail.com>
2019-06-23 15:40:26 +0200

    Correctly return impl Iterator

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,16 @@ pub fn suggestions(input: &str) -> impl Iterator<Item = &str> {
   let sub_strings: Vec<&str> = input.splitn(2, "@").collect();
 
   if sub_strings.len() < 2 || sub_strings[0].is_empty() {
-    return std::iter::empty::<&str>();
+    None
+  } else {
+    let parsed_domain_prefix = sub_strings[1];
+    Some(
+      DEFAULT_DOMAINS
+        .iter()
+        .filter(move |domain| domain.starts_with(parsed_domain_prefix))
+        .map(|domain| *domain),
+    )
   }
-
-  let domain = sub_strings[1];
-  return DEFAULT_DOMAINS.iter().filter(|d| d.starts_with(domain));
+  .into_iter()
+  .flat_map(|suggestions| suggestions)
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,64 +1,88 @@
 use email_suggestions::*;
 
 #[test]
-fn suggestions_returns_no_suggestions_for_input_without_at_sign() {
-  let result: Vec<&str> = vec![];
-  assert_eq!(suggestions(""), result);
-  assert_eq!(suggestions("foo"), result);
+fn returns_no_suggestions_for_input_without_at_sign() {
+  let expected_result: Vec<&str> = vec![];
+  let mut actual_result: Vec<&str>;
+
+  actual_result = suggestions("").collect();
+  assert_eq!(actual_result, expected_result);
+
+  actual_result = suggestions("foo").collect();
+  assert_eq!(actual_result, expected_result);
 }
 
 #[test]
-fn suggestions_returns_no_suggestions_for_input_without_local_part() {
-  let result: Vec<&str> = vec![];
-  assert_eq!(suggestions("@"), result);
-  assert_eq!(suggestions("@g"), result);
+fn returns_no_suggestions_for_input_without_local_part() {
+  let expected_result: Vec<&str> = vec![];
+  let mut actual_result: Vec<&str>;
+
+  actual_result = suggestions("@").collect();
+  assert_eq!(actual_result, expected_result);
+
+  actual_result = suggestions("@g").collect();
+  assert_eq!(actual_result, expected_result);
 }
 
 #[test]
-fn suggestions_returns_no_suggestions_for_input_with_multiple_at_signs() {
-  let result: Vec<&str> = vec![];
-  assert_eq!(suggestions("foo@@"), result);
-  assert_eq!(suggestions("foo@bar@baz"), result);
-  assert_eq!(suggestions("foo@bar@baz@"), result);
+fn returns_no_suggestions_for_input_with_multiple_at_signs() {
+  let expected_result: Vec<&str> = vec![];
+  let mut actual_result: Vec<&str>;
+
+  actual_result = suggestions("foo@@").collect();
+  assert_eq!(actual_result, expected_result);
+
+  actual_result = suggestions("foo@bar@baz").collect();
+  assert_eq!(actual_result, expected_result);
+
+  actual_result = suggestions("foo@bar@baz@").collect();
+  assert_eq!(actual_result, expected_result);
 }
 
 #[test]
-fn suggestions_returns_no_suggestions_for_non_matching_domains() {
-  let result: Vec<&str> = vec![];
-  assert_eq!(suggestions("foo@bar"), result);
+fn returns_no_suggestions_for_non_matching_domains() {
+  let expected_result: Vec<&str> = vec![];
+  let actual_result: Vec<&str> = suggestions("foo@bar").collect();
+  assert_eq!(actual_result, expected_result);
 }
 
 #[test]
-fn suggestions_returns_default_domains_for_input_ending_with_single_at_sign() {
-  assert_eq!(
-    suggestions("foo@"),
-    vec![
-      "aol.com",
-      "gmail.com",
-      "google.com",
-      "yahoo.com",
-      "ymail.com",
-      "hotmail.com",
-      "live.com",
-      "outlook.com",
-      "inbox.com",
-      "mail.com",
-      "gmx.com",
-      "icloud.com",
-    ]
-  );
+fn returns_default_domains_for_input_ending_with_single_at_sign() {
+  let expected_result: Vec<&str> = vec![
+    "aol.com",
+    "gmail.com",
+    "google.com",
+    "yahoo.com",
+    "ymail.com",
+    "hotmail.com",
+    "live.com",
+    "outlook.com",
+    "inbox.com",
+    "mail.com",
+    "gmx.com",
+    "icloud.com",
+  ];
+  let actual_result: Vec<&str> = suggestions("foo@").collect();
+  assert_eq!(actual_result, expected_result);
 }
 
 #[test]
-fn suggestions_returns_all_matching_domains() {
-  assert_eq!(
-    suggestions("foo@g"),
-    vec!["gmail.com", "google.com", "gmx.com"]
-  );
+fn returns_all_matching_domains() {
+  let expected_result: Vec<&str> = vec!["gmail.com", "google.com", "gmx.com"];
+  let actual_result: Vec<&str> = suggestions("foo@g").collect();
+  assert_eq!(actual_result, expected_result);
 }
 
 #[test]
-fn suggestionsn_returns_at_most_n_matching_domains() {
-  assert_eq!(suggestionsn(2, "foo@g"), vec!["gmail.com", "google.com"]);
-  assert_eq!(suggestionsn(5, "foo@y"), vec!["yahoo.com", "ymail.com"]);
+fn returns_results_that_can_be_limited_with_take() {
+  let mut expected_result: Vec<&str>;
+  let mut actual_result: Vec<&str>;
+
+  actual_result = suggestions("foo@g").take(2).collect();
+  expected_result = vec!["gmail.com", "google.com"];
+  assert_eq!(actual_result, expected_result);
+
+  actual_result = suggestions("foo@y").take(5).collect();
+  expected_result = vec!["yahoo.com", "ymail.com"];
+  assert_eq!(actual_result, expected_result);
 }
