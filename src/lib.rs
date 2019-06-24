391468d0e241348c12a7bd380449cb9c914e7027
Can Göktas <can.goektas@gmail.com>
2019-06-24 22:57:38 +0200

    Fix implementation with EmailSuggestion struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-static DEFAULT_DOMAINS: &[&str] = &[
+static DEFAULT_DOMAINS: &'static [&'static str] = &[
   "aol.com",
   "gmail.com",
   "google.com",
@@ -13,13 +13,13 @@ static DEFAULT_DOMAINS: &[&str] = &[
   "icloud.com",
 ];
 
-pub struct EmailSuggestion<'a> {
-  complete_address: &'a str,
-  text_to_append: &'a str,
+pub struct EmailSuggestion {
+  pub complete_address: String,
+  pub text_to_append: String,
 }
 
-impl<'a> EmailSuggestion<'a> {
-  fn new(complete_address: &'a str, text_to_append: &'a str) -> EmailSuggestion<'a> {
+impl EmailSuggestion {
+  fn new(complete_address: String, text_to_append: String) -> EmailSuggestion {
     EmailSuggestion {
       complete_address,
       text_to_append,
@@ -30,24 +30,27 @@ impl<'a> EmailSuggestion<'a> {
 pub fn suggestions<'a>(
   input: &'a str,
   domains: Option<&'a [&'a str]>,
-) -> impl Iterator<Item = EmailSuggestion<'a>> {
-  let sub_strings: Vec<&str> = input.splitn(2, "@").collect();
+) -> impl Iterator<Item = EmailSuggestion> + 'a {
+  let sub_strings: Vec<&'a str> = input.splitn(2, "@").collect();
 
   if sub_strings.len() < 2 || sub_strings[0].is_empty() {
     None
   } else {
-    let parsed_domain_prefix = sub_strings[1];
+    let parsed_domain_prefix: &'a str = sub_strings[1];
+    let domains: &'a [&'a str] = domains.unwrap_or(DEFAULT_DOMAINS);
     Some(
       domains
-        .unwrap_or(DEFAULT_DOMAINS)
         .iter()
-        .filter(move |domain| domain.starts_with(parsed_domain_prefix))
+        .filter(move |domain| {
+          parsed_domain_prefix.len() < domain.len() && domain.starts_with(parsed_domain_prefix)
+        })
         .map(move |domain| &domain[parsed_domain_prefix.len()..])
-        .filter(|text_to_append| !text_to_append.is_empty())
         .map(move |text_to_append| {
-          let mut complete_address = input.to_owned();
+          let mut complete_address: String = input.to_owned();
           complete_address.push_str(text_to_append);
-          EmailSuggestion::new(&complete_address, text_to_append)
+          let text_to_append: String = text_to_append.to_owned();
+
+          EmailSuggestion::new(complete_address, text_to_append)
         }),
     )
   }
